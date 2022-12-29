17065b334263d3a9819d7fb3d78ac32e6799bdaf
Flutter-D-team <owork0819@gamil.com>
2022-12-29 12:25:34 +0530

    ads

--- wallpaper_a/lib/HomePage.dart
+++ wallpaper_a/lib/HomePage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -8,6 +9,31 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  NativeAd? nativeAd;
+  bool isLoaded = false;
+
+  @override
+  void didChangeDependencies() {
+    // TODO: implement didChangeDependencies
+    super.didChangeDependencies();
+    nativeAd = NativeAd(
+      adUnitId: "ca-app-pub-3940256099942544/1044960115",
+      factoryId: "listTile",
+      listener: NativeAdListener(onAdLoaded: (_) {
+        setState(() {
+          isLoaded = true;
+        });
+        print("Ad Loaded");
+      }, onAdFailedToLoad: (ad, error) {
+        print("Ad Failed to load");
+        ad.dispose();
+      }),
+      request: const AdRequest(),
+    );
+    // We Have Created Native Ad Now, Let's Load it
+    nativeAd!.load();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,7 +41,14 @@ class _HomePageState extends State<HomePage> {
         centerTitle: true,
         title: Text("Home Screen"),
       ),
-      body: Container(),
+      body: Container(
+        height: 500,
+        child: isLoaded
+            ? AdWidget(
+                ad: nativeAd!,
+              )
+            : Center(child: Text("Ad is Loading")),
+      ),
     );
   }
 }
